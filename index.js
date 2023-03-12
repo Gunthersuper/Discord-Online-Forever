23a3a02acf2f359620bd1ef3e266142d4eb08d90
Gunther <62240649+Gunthersuper@users.noreply.github.com>
2023-03-12 23:30:51 +0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const Eris = require("eris");
+const keep_alive = require('./keep_alive.js')
+
+// Replace TOKEN with your bot account's token
+const bot = new Eris(process.env.token);
+
+bot.on("error", (err) => {
+  console.error(err); // or your preferred logger
+});
+
+bot.connect(); // Get the bot to connect to Discord
