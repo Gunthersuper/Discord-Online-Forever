41d617e8e326a66fe8bfa968d5caa51bd50be340
Murat YILMAZ <murattnt@live.com>
2024-02-20 05:37:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,4 +8,9 @@ bot.on("error", (err) => {
   console.error(err); // or your preferred logger
 });
 
+bot.on("ready", () => {
+  console.log("Ready!");
+  bot.editStatus("idle", {name: "Blob Project", type: 1, url: "https://twitch.tv/discord"})
+})
+
 bot.connect(); // Get the bot to connect to Discord
